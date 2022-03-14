9f0adcdf2351a924cfbe9daed4202add77ea29d9
jcortezll <joluenti@gmail.com>
2022-03-14 09:47:15 +0100

    Funcionen tots els tests inicials

--- Calc-TDD/src/OperacionesTDD.java
+++ Calc-TDD/src/OperacionesTDD.java
@@ -1,6 +1,13 @@
 public class OperacionesTDD {
     public static int suma(String s) {
-        if(s.equals("1")) return 1;
-        return 0;
+        int res=0;
+        if(s.equals("1")) res=1;
+        else if(s.length()>1){
+            for(int i=0;i < s.split(",").length;i++){
+                res += Integer.parseInt(s.split(",")[i]);
+            }
+        }
+        return res;
     }
 }
+
--- Calc-TDD/src/OperacionesTDDTest.java
+++ Calc-TDD/src/OperacionesTDDTest.java
@@ -8,5 +8,7 @@ class OperacionesTDDTest {
     void Test(){
         assertEquals(0,OperacionesTDD.suma(""));
         assertEquals(1,OperacionesTDD.suma("1"));
+        assertEquals(3,OperacionesTDD.suma("1,2"));
+        assertEquals(4,OperacionesTDD.suma("1,1,2"));
     }
 }
