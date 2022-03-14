24421936ff1ebcee230ac7b1a6424b725915ff51
jcortezll <joluenti@gmail.com>
2022-03-14 09:22:30 +0100

    Ultims test de FizzBuzz que funcionen i cree el módul operacionesTDD

--- tasca1-fizzbuzz/src/FizzBuzz.java
+++ tasca1-fizzbuzz/src/FizzBuzz.java
@@ -1,6 +1,7 @@
 public class FizzBuzz {
 
     public static String genera(int i) {
+        if(i%5==0 && i%3==0) return "FizzBuzz";
         if(i%5==0) return "Buzz";
         if(i%3==0) return "Fizz";
         return i + "";
--- tasca1-fizzbuzz/src/FizzBuzzTest.java
+++ tasca1-fizzbuzz/src/FizzBuzzTest.java
@@ -31,4 +31,17 @@ class FizzBuzzTest {
     void Test10(){
         assertEquals("Buzz",FizzBuzz.genera(10));
     }
+
+    @Test
+    void Test_11_12_13_14(){
+        assertEquals("11",FizzBuzz.genera(11));
+        assertEquals("Fizz",FizzBuzz.genera(12));
+        assertEquals("13",FizzBuzz.genera(13));
+        assertEquals("14",FizzBuzz.genera(14));
+    }
+
+    @Test
+    void Test15(){
+        assertEquals("FizzBuzz",FizzBuzz.genera(15));
+    }
 }
