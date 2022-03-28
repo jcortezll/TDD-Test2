39a64d4fc22a82c84bb5ea0a60263550c2e6194e
jcortezll <joluenti@gmail.com>
2022-03-28 09:51:19 +0200

    Simplificat la expresió condicional del primer if

--- TASCA REFACTOR– GIT 6/src/CovidIFs.java
+++ TASCA REFACTOR– GIT 6/src/CovidIFs.java
@@ -5,22 +5,7 @@ public class CovidIFs {
                                boolean kidnevDisease, boolean respiratoryDisease, String provincia
     ){
         String diagnostico;
-        if (
-                (bodyTemperature>= 38 && difficultyBreathing) ||
-                        (bodyTemperature>= 38 && difficultyBreathing && diabetes) ||
-                        (bodyTemperature>= 38 && difficultyBreathing && cancer) ||
-                        (bodyTemperature>= 38 && difficultyBreathing && isPregnant)||
-                        (bodyTemperature>= 38 && difficultyBreathing && isOver60yearsold) ||
-                        (bodyTemperature>= 38 && difficultyBreathing && hepatic)||
-                        (bodyTemperature>= 38 && difficultyBreathing && kidnevDisease)||
-                        (bodyTemperature>= 38 && difficultyBreathing && respiratoryDisease)||
-                        (bodyTemperature>= 38 && diabetes)||
-                        (bodyTemperature>= 38 && cancer)||
-                        (bodyTemperature>= 38 && isPregnant)||
-                        (bodyTemperature>= 38 && isOver60yearsold)||
-                        (bodyTemperature>= 38 && hepatic)||
-                        (bodyTemperature>= 38 && kidnevDisease)||
-                        (bodyTemperature>= 38 && respiratoryDisease)){
+        if (bodyTemperature>= 38 && (difficultyBreathing || diabetes || cancer || isPregnant || isOver60yearsold || hepatic || kidnevDisease || respiratoryDisease)){
             diagnostico = "/diagnostico/"+provincia;
         }
         else if(bodyTemperature>= 38){
