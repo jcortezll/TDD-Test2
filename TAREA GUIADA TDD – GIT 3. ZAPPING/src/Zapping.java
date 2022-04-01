18734ea8c0bb04ec2d9881601577c04b0252ed2c
jcortezll <joluenti@gmail.com>
2022-04-01 09:05:14 +0200

    Segon test

--- TAREA GUIADA TDD – GIT 3. ZAPPING/src/Zapping.java
+++ TAREA GUIADA TDD – GIT 3. ZAPPING/src/Zapping.java
@@ -1,8 +1,10 @@
 public class Zapping {
         public static int cuantosClics(int origen, int destino){
-            int res=-1;
+            int res;
             if(origen==destino)
                 res=0;
+            else
+                res=destino-origen;
             return res;
         }
 }
--- TAREA GUIADA TDD – GIT 3. ZAPPING/src/ZappingTest.java
+++ TAREA GUIADA TDD – GIT 3. ZAPPING/src/ZappingTest.java
@@ -5,9 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class ZappingTest {
 
     @Test
-    public void test1(){
+    void test1(){
         int expected=0;
         assertEquals(expected,Zapping.cuantosClics(0,0));
     }
-
+    @Test
+    void test2(){
+        int expected=1;
+        assertEquals(expected,Zapping.cuantosClics(1,2));
+    }
 }
