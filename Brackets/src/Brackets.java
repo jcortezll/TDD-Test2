ea532a1c692c922d072589169f88ea93582ad0b3
jcortezll <joluenti@gmail.com>
2022-04-11 09:43:34 +0200

    Verify refactorizado

--- Brackets/src/Brackets.java
+++ Brackets/src/Brackets.java
@@ -2,29 +2,21 @@ public class Brackets {
     public boolean verify(String cadena){
         boolean verified=false,seguir=true;
         int cont1=0,cont2=0;
-        if(!cadena.equals("")) {
-            for (int i = 0; i < cadena.length(); i++) {
-                if (cadena.charAt(i) == '(')
-                    cont1++;
-                if (cadena.charAt(i) == ')')
-                    cont2++;
-                if(cont2>cont1){
-                    seguir=false;
-                    verified=false;
-                    break;
-                }
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == '(') cont1++;
+            if (cadena.charAt(i) == ')') cont2++;
+            if(cont2>cont1){
+                seguir=false;
+                verified=false;
+                break;
             }
-            if(seguir) {
-                if (cont1 == 0 || cont2 == 0) {
-                    verified = false;
-                } else if (cont1 == cont2) {
-                    verified = true;
-                } else {
-                    verified = false;
-                }
+        }
+        if(seguir) {
+            if (cont1 == 0 && cont2 == 0) {
+                verified = true;
+            } else if (cont1 == cont2){
+                verified = true;
             }
-        }else{
-            verified=true;
         }
         return verified;
     }
